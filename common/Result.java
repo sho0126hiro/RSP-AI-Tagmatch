54f19f05ca7386171a9924981b7ee3ac0dcea247
sho0126hiro <sho0126hiro@gmail.com>
2021-01-15 15:11:45 +0900

    marge

--- common/Result.java
+++ common/Result.java
@@ -1,14 +1,13 @@
 package common;
 
 /**
- *　対戦結果
+ * 対戦結果
  */
 public class Result {
     public StatusEnum status; // 勝敗を示すフラグ
 
     /**
-     * StatusEnumは分岐用
-     * 実際は勝利数（総獲得ポイント）で争うので，このEnumの勝利数（チーム全体としての勝利数）ではないので注意
+     * StatusEnumは分岐用 実際は勝利数（総獲得ポイント）で争うので，このEnumの勝利数（チーム全体としての勝利数）ではないので注意
      */
     public enum StatusEnum {
         WIN, LOSE, DRAW
@@ -26,29 +25,28 @@ public class Result {
     public TagTeamAction AllyTeamAction; // 今回出したAが出した手
     public TagTeamAction EnemyTeamAction; // 今回出したBが出した手
 
-    public Result(int pointOfAllyAgentA, int pointOfAllyAgentB, 
-        int pointOfEnemyAgentA, int pointOfEnemyAgentB, 
-        TagTeamAction tagTeamActionA, TagTeamAction tagTeamActionB){
-            this.PointOfAllyAgentA = pointOfAllyAgentA;
-            this.PointOfAllyAgentB = pointOfAllyAgentB;
-            this.PointOfEnemyAgentA = pointOfEnemyAgentA;
-            this.PointOfEnemyAgentB = pointOfEnemyAgentB;
-            this.allyPoint = this.PointOfAllyAgentA + this.PointOfAllyAgentB;
-            this.enemyPoint = this.PointOfEnemyAgentA + this.PointOfEnemyAgentB;
-            this.AllyTeamAction = tagTeamActionA;
-            this.EnemyTeamAction = tagTeamActionB;
-            if(this.allyPoint > this.enemyPoint){
-                this.status = StatusEnum.WIN;
-            }else if(this.allyPoint == this.enemyPoint){
-                this.status = StatusEnum.DRAW;
-            }else {
-                this.status = StatusEnum.LOSE;
-            }
+    public Result(int pointOfAllyAgentA, int pointOfAllyAgentB, int pointOfEnemyAgentA, int pointOfEnemyAgentB,
+            TagTeamAction tagTeamActionA, TagTeamAction tagTeamActionB) {
+        this.PointOfAllyAgentA = pointOfAllyAgentA;
+        this.PointOfAllyAgentB = pointOfAllyAgentB;
+        this.PointOfEnemyAgentA = pointOfEnemyAgentA;
+        this.PointOfEnemyAgentB = pointOfEnemyAgentB;
+        this.allyPoint = this.PointOfAllyAgentA + this.PointOfAllyAgentB;
+        this.enemyPoint = this.PointOfEnemyAgentA + this.PointOfEnemyAgentB;
+        this.AllyTeamAction = tagTeamActionA;
+        this.EnemyTeamAction = tagTeamActionB;
+        if (this.allyPoint > this.enemyPoint) {
+            this.status = StatusEnum.WIN;
+        } else if (this.allyPoint == this.enemyPoint) {
+            this.status = StatusEnum.DRAW;
+        } else {
+            this.status = StatusEnum.LOSE;
+        }
     }
 
     @Override
-    public String toString(){
-        return String.format("[status: %s] [Ally] A: %d, B: %d [Enemy] A: %d, B: %d", 
-        this.status.name(), this.PointOfAllyAgentA, this.PointOfAllyAgentB, this.PointOfEnemyAgentA, this.PointOfEnemyAgentB);
+    public String toString() {
+        return String.format("[status: %s] [Ally] A: %d, B: %d [Enemy] A: %d, B: %d", this.status.name(),
+                this.PointOfAllyAgentA, this.PointOfAllyAgentB, this.PointOfEnemyAgentA, this.PointOfEnemyAgentB);
     }
 }
