da843e9bf96a9517e2b0de36f5e3d00932038fc6
Sho Hirose <sho0126hiro@gmail.com>
2021-02-07 06:29:37 +0900

    Revert "Souatari"

---
+++ Runner.java
@@ -0,0 +1,12 @@
+
+/**
+ * 実行クラス
+ */
+public class Runner {
+    public static void main(String[] args){
+        
+        BattleManager bm = new BattleManager();
+        bm.run();
+        
+    }
+}
--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-cd src/
 rm teams/*.class
 rm agents/*.class
 rm common/*.class
@@ -9,7 +7,4 @@ javac Runner.java
 javac agents/*.java
 javac teams/*.java
 javac common/*.java
-# ここに --ignores-aiko を入れるとあいこ分を表示しない
-# ここに --ignores-logs を入れると途中経過を表示しない
-# ここに --result を入れるとリザルトを表示
-java Runner "$@"
+java Runner
