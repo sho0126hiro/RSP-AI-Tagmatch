1c150cf3ed815d4a36c191cd5fc811b572e06553
KimotoYanke <kimotoyanke@gmail.com>
2021-02-07 13:16:04 +0900

    Revert "Merge pull request #16 from KimotoYanke/souatari"
    This reverts commit 38962558052b538fdbe84dcfdddfcfebbad5f91a, reversing
    changes made to 5bb249c3182fa6f9755e3fab2265ac758c8ff7c4.

--- Runner.java
+++ Runner.java
@@ -1,14 +1,11 @@
 
-import java.util.Arrays;
 /**
  * 実行クラス
  */
 public class Runner {
     public static void main(String[] args){
-        boolean ignoresAiko = Arrays.asList(args).contains("--ignores-aiko");
-        boolean ignoresLogs = Arrays.asList(args).contains("--ignores-logs");
-        boolean result = Arrays.asList(args).contains("--result");
-        BattleManager bm = new BattleManager(ignoresAiko, ignoresLogs, result);
+        
+        BattleManager bm = new BattleManager();
         bm.run();
         
     }
